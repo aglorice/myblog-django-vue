7f8dd188c1a9564e9ab92c4c162ef25edff56314
aglorice <401208941@qq.com>
2023-01-09 01:05:04 +0800

    重构了页面

--- src/views/category.vue
+++ src/views/category.vue
@@ -3,22 +3,65 @@
     <div class="container_head">
       <h1>分类</h1>
     </div>
-    <categorycloud></categorycloud>
+    <div class="category_list">
+      <h4>文章分类</h4>
+      <p>目前共计{{echartsData.length}}个分类</p>
+      <ul class="category_list_item" v-for="(category_item) in echartsData" :key="category_item.name">
+        <li>
+          <router-link :to="`/article/${category_item.name}/`">{{category_item.name}}</router-link>
+          <span>({{category_item.value}})</span>
+        </li>
+      </ul>
+    </div>
+
   </div>
 
 </template>
 
 <script>
-import categorycloud from "@/components/categorycloud";
+import {getCountCategorize} from "@/api/http";
+
 export default {
   name: `category`,
+  data(){
+    return{
+      echartsData:0
+    }
+  },
   components:{
-    categorycloud
+
+  },
+  mounted: function () {
+    this.getcountcategorize()
+  },
+  methods: {
+    getcountcategorize() {
+      getCountCategorize(null).then((res) => {
+        if (res.code === 200) {
+          this.echartsData = res['context']
+          // 将信息提交到vuex
+        } else {
+          this.$message({
+            type: 'info',
+            message: '数据获取失败',
+            duration: 1500
+          });
+        }
+      }).catch((err) => {
+        console.log(err)
+      })
+    },
   }
 }
 </script>
 
 <style scoped lang="scss">
+@import "@/assets/scss/_handle.scss";
+@media (max-width: 750px) {
+  .category_list_item {
+    width: 90%!important;
+  }
+}
 .container_head {
   width: 100vw;
   height: 60vh;
@@ -41,7 +84,50 @@ h1 {
 
 }
 .container_head {
-  margin-bottom: 20px;
-}
 
+}
+.category_list {
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  @include background_color("background_color1");
+  @include font_color("font_color1");
+  h4 {
+    margin-top:  20px;
+  }
+}
+.category_list_item {
+  li {
+    padding-left: 2em;
+    position: relative;
+    list-style: none;
+    a {
+      @include font_color("font_color1");
+      &:hover {
+        color: skyblue;
+      }
+    }
+    span {
+      color: #898a8a;
+    }
+  }
+  li::before {
+    top:0.28em;
+    left: 0;
+    content: ''; // 伪类生效必须加上这个
+    position: absolute;
+    width: 1em;
+    height: 1em;
+    background: transparent;
+    border-radius: 0.5em;
+    border: 0.3em skyblue solid;
+  }
+  li:hover:before {
+    border: 0.3em darkorange solid;
+  }
+
+  width: 70%;
+}
 </style>
