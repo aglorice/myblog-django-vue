f76abd23d3f2c0041f3bb394b05b55aa09c71225
aglorice <401208941@qq.com>
2023-01-09 13:39:01 +0800

    新增标签页

---
+++ src/views/pagArticle.vue
@@ -0,0 +1,98 @@
+<template>
+  <div>
+    <div class="container_head">
+      <h1>标签</h1>
+      <h4>{{this.$route.params.pag}}</h4>
+    </div>
+    <div class="container_body">
+      <h4>一共发现了{{pag.length}}篇文章</h4>
+      <div class="category_type">
+        <el-timeline>
+          <el-timeline-item v-for="(value) in pag"
+                            :timestamp="value.created_time"
+                            :key="value.id"
+                            placement="top">
+            <el-card >
+              <router-link  :to="`/article/details/${value.id}`"><h4 class="datetime-title-id">{{value.title}}</h4></router-link>
+            </el-card>
+          </el-timeline-item>
+        </el-timeline>
+      </div>
+
+    </div>
+  </div>
+</template>
+
+<script>
+
+import {article_type} from "@/utils/category_type";
+
+export default {
+  name: `pagArticle`,
+  data(){
+    return{
+      pag:[]
+    }
+  },
+  mounted() {
+    this.articleType()
+  },
+  methods:{
+    articleType(){
+      this.pag = article_type(this.$store.state.OriginalArticles,this.$route.params.pag,'pag')
+      console.log(this.pag)
+    }
+  }
+
+}
+</script>
+
+<style scoped lang="scss">
+@import "@/assets/scss/_handle.scss";
+@media (max-width: 750px) {
+  .category_type {
+    width: 90% !important;
+  }
+}
+.container_head {
+  width: 100vw;
+  height: 60vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-image: url("@/assets/img/backgound-img/index.png");
+  background-position: center center;
+  /* 背景图不平铺 */
+  background-repeat: no-repeat;
+  /* 当内容高度大于图片高度时，背景图像的位置相对于viewport固定 */
+  //background-attachment: fixed;
+  /* 让背景图基于容器大小伸缩 */
+  background-size: cover;
+
+  h1 {
+    font-size: 3em;
+    color: white;
+  }
+  h4 {
+    color: white;
+  }
+}
+.container_body {
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  @include background_color("background_color1");
+  @include font_color("font_color1");
+  h4 {
+    margin-top: 20px;
+    font-family: Noto Serif SC,serif;
+  }
+}
+.category_type {
+  margin-top: 20px;
+  width: 70%;
+}
+</style>
