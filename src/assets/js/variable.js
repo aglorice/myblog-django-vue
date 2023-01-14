13341126c087875cebe19c5c4b3d42855e56a8de
aglorice <401208941@qq.com>
2023-01-14 15:17:23 +0800

    修改后端路由

--- src/assets/js/variable.js
+++ src/assets/js/variable.js
@@ -1,6 +1,6 @@
 
 const variable = {
-    base_url : 'http://127.0.0.1:8000', // https://www.aglorice.cn
+    base_url : 'http://81.68.83.97/', // https://www.aglorice.cn
     base_url_img : '',
     websiteDatetime:'2022-1-12' // 网站开始运行的时间
 }
