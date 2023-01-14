1a253f1fa5be55c4887158fc5cb223518864bdbe
aglorice <401208941@qq.com>
2023-01-14 22:12:02 +0800

    新增网站地址变量

--- src/assets/js/variable.js
+++ src/assets/js/variable.js
@@ -1,7 +1,8 @@
 
 const variable = {
-    base_url : 'http://81.68.83.97/', // https://www.aglorice.cn
+    base_url : 'http://127.0.0.1:8000/', // https://www.aglorice.cn
     base_url_img : '',
-    websiteDatetime:'2022-1-12' // 网站开始运行的时间
+    websiteDatetime:'2022-1-12', // 网站开始运行的时间
+    selfWbsite:'http://127.0.0.1:8080'
 }
 export  default variable
