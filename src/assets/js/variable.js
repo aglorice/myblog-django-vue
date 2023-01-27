c4f8ab9ce03003ef48807ac1ef8ee5f5d1ec1e74
aglorice <401208941@qq.com>
2023-01-27 23:12:52 +0800

    修改后端地址

--- src/assets/js/variable.js
+++ src/assets/js/variable.js
@@ -1,8 +1,8 @@
 
 const variable = {
-    base_url : 'https://www.aglorice.cn/', //后端接口
+    base_url : 'https://www.aglorice.cn', //后端接口
     base_url_img : '',
     websiteDatetime:'2023-1-12', // 网站开始运行的时间
-    selfWbsite:'https://aglirice.cn'
+    selfWbsite:'https://aglorice.cn'
 }
 export  default variable
