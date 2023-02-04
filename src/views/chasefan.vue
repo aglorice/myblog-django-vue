72ed85fc1758406366da8f60730b42a1ca584703
aglorice <401208941@qq.com>
2023-02-04 13:03:02 +0800

    新增番剧分类功能

--- src/views/chasefan.vue
+++ src/views/chasefan.vue
@@ -8,20 +8,27 @@
     <h1 >追番列表</h1>
     <el-skeleton class="loading" v-show="loading" :rows="6" animated :throttle="500" />
     <div class="fans row">
-        <el-card class="fans-item box-card container-body-card col-xs-6  col-md-4 col-lg-3"
-                 v-for="(value) in fansData" :key="value.media_id">
-          <a style="text-decoration: none" :href="value.url">
-            <div slot="header" class="clearfix">
-<!--              注意这里修改了v-lazy的源码，已实现在不改变网站的referer的情况下也能加载图片，原因是因为v-lazy使用了虚拟dom的操作-->
-              <img v-lazy="value.cover" :alt="value.title">
-            </div>
-            <div class="box-card-body">
-              <h5>{{value.title}}</h5>
-            </div>
-          </a>
+    <el-tabs v-model="activeName" @tab-click="handleClick">
+      <el-tab-pane v-for="(labelName,index) in optionsName " :label="labelName" :name="labelName" :key="index">
+        <div class="fans-item-pane row">
+          <el-card class="fans-item box-card container-body-card col-xs-6  col-md-4 col-lg-3"
+                   v-for="(value) in typeFansData" :key="value.media_id">
+            <a style="text-decoration: none" :href="value.url">
+              <div slot="header" class="clearfix">
+                <!--              注意这里修改了v-lazy的源码，已实现在不改变网站的referer的情况下也能加载图片，原因是因为v-lazy使用了虚拟dom的操作-->
+                <img v-lazy="value.cover" :alt="value.title">
+              </div>
+              <div class="box-card-body">
+                <h5>{{value.title}}</h5>
+              </div>
+            </a>
 
-        </el-card>
+          </el-card>
+        </div>
+      </el-tab-pane>
+    </el-tabs>
     </div>
+
   </div>
 </div>
 </template>
@@ -35,7 +42,9 @@ export default {
   data(){
     return{
       fansData:'',
-      loading:true
+      loading:true,
+      activeName: '全部',
+      optionsName:['全部','连载中','已完结','番剧','电影']
     }
   },
   mounted() {
@@ -46,6 +55,31 @@ export default {
     // stopp() // 停止樱花
     document.getElementById('canvas_sakura').style.display = 'none'
   },
+  computed:{
+    typeFansData:function (){
+      switch (this.activeName){
+        case this.optionsName[0]:
+          return this.fansData;
+        case this.optionsName[1]:
+          return this.fansData.filter(item =>{
+            return item.is_finish == 0
+          })
+        case this.optionsName[2]:
+          return this.fansData.filter(item =>{
+            return item.is_finish == 1
+          })
+        case this.optionsName[3]:
+          return this.fansData.filter(item =>{
+            return item.season_version == 'tv'
+          })
+        case this.optionsName[4]:
+          return this.fansData.filter(item =>{
+            return item.season_version == 'movie'
+          })
+      }
+      return this.fansData;
+    }
+  },
   methods:{
     // 开启樱花特效
     myStartSakura(){
@@ -55,6 +89,10 @@ export default {
         startSakura() // 樱花特效
       }
 
+    },
+    // 处理标签页移动
+    handleClick(tab, event) {
+      console.log(tab.index, event);
     },
     getFans(){
       let param = {
@@ -174,6 +212,9 @@ export default {
 .loading {
   width: 70%!important;
   height: 20em!important;
+}
+.fans-item-pane {
+  width: 100%;
 }
 
 </style>
