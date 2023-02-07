c66624cec8f1770c2c77835628927e99e4077e52
aglorice <401208941@qq.com>
2023-02-07 12:10:16 +0800

    无

--- src/assets/js/variable.js
+++ src/assets/js/variable.js
@@ -1,17 +1,17 @@
-
-const variable = {
-    base_url : 'http://localhost:8000', //后端接口
-    base_url_img : '',
-    websiteDatetime:'2023-1-12', // 网站开始运行的时间
-    selfWbsite:'https://aglorice.cn'
-}
-
+//
 // const variable = {
-//     base_url : 'https://www.aglorice.cn', //后端接口
+//     base_url : 'http://localhost:8000', //后端接口
 //     base_url_img : '',
 //     websiteDatetime:'2023-1-12', // 网站开始运行的时间
 //     selfWbsite:'https://aglorice.cn'
 // }
+
+const variable = {
+    base_url : 'https://www.aglorice.cn', //后端接口
+    base_url_img : '',
+    websiteDatetime:'2023-1-12', // 网站开始运行的时间
+    selfWbsite:'https://aglorice.cn'
+}
 
 
 export  default variable
