432510aa9efeb9329841723cdca0a5b7bdd97663
aglorice <401208941@qq.com>
2023-02-12 18:24:04 +0800

    解决了浏览器进程卡死的问题

--- src/components/mobile-aplayer.vue
+++ src/components/mobile-aplayer.vue
@@ -5,12 +5,15 @@
           controls
           @ended="musicEnd"
           ref="myaudio"
-          preload="auto"
+
           style="display:none;">
+    <source ref="test" id="test" :src=
+        "currentMusic.url"
+            type="audio/mp3">
   </audio>
   <div class="my_aplayer">
-    <img class="music_cover" :src="currentMusic.cover" alt="">
-    <h5>{{currentMusic.name}}-{{currentMusic.artist}}</h5>
+    <img id="cover" :class="{'toggleAudioAni':true,'pause':!isPlay}" :src="currentMusic.cover" alt="">
+    <h5 :class="{'animate':isPlay,'text_pause':!isPlay}">{{currentMusic.name}}-{{currentMusic.artist}}</h5>
     <div class="music_options">
       <i @click="prev" class="el-icon-d-arrow-left"></i>
       <i  @click="myplay" :class="isPlay?'el-icon-video-pause':'el-icon-video-play'"></i>
@@ -33,11 +36,13 @@ export default {
       allMusic:0,
       audio: [], // 音乐列表
       currentMusic :{},
-      isPlay:false // 是否播放
+      isPlay:false, // 是否播放
+      ispause:true,
     }
   },
   mounted() {
       this.getmusic()
+      this.$refs.test.play()
   },
   methods: {
     getmusic(){
@@ -58,31 +63,39 @@ export default {
     },
     prev(){
       this.currentMusic = this.audio[this.current - 1 === -1?this.current =this.allMusic - 1:--this.current]
-      let playPromise = this.$refs.myaudio.play();
-      if (playPromise !== undefined) {
-        playPromise.then(() => {
-          this.$refs.myaudio.load();
-          if(this.isPlay === true){
-            this.$refs.myaudio.play();
-          }
-        }).catch(() => {
-          this.next()
-        });
+      this.$refs.myaudio.oncanplay = ()=>{
+        if (this.isPlay){
+          this.$refs.myaudio.play();
+        }
       }
     },
     next(){
       this.currentMusic = this.audio[this.current+1 === this.allMusic?this.current = 0:++this.current]
-      let playPromise = this.$refs.myaudio.play();
-      if (playPromise !== undefined) {
-        playPromise.then(() => {
-          this.$refs.myaudio.load();
-          if(this.isPlay === true){
-            this.$refs.myaudio.play();
-          }
-        }).catch(() => {
-              this.next()
-        });
+      // 在没有使用source的情况切换音频是不需要使用load来加载音频的，当只把audio中的src的资源切换后audio会自动进行加载
+      // 如果你在这里直接调用play()方法的话，那样将会发生错误，很大概率会出现音频还会处于播放状态下就开始播放了，这显然是错误的
+      // 这里我们可以选择捕捉这个错误，因为play()方法会有返回值
+      // DOMException: The play() request was interrupted by a new load request.
+      // 如果使用source需要手动load
+      this.$refs.myaudio.oncanplay = ()=>{
+        if (this.isPlay){
+          this.$refs.myaudio.play()
+        }
       }
+      // let playPromise = this.$refs.myaudio.play();
+      //
+      // if (playPromise !== undefined) {
+      //   playPromise.then(() => {
+      //     this.$refs.myaudio.load();
+      //     if(this.isPlay === true){
+      //       this.$refs.myaudio.play();
+      //       this.ispause = false
+      //     }
+      //   }).catch(() => {
+      //     debugger
+      //     console.log('1234234')
+      //         this.next()
+      //   });
+      // }
     },
     myplay(){
       if (!this.isPlay){
@@ -90,6 +103,7 @@ export default {
       }else {
         this.$refs.myaudio.pause()
       }
+      this.ispause = !this.isPlay
       this.isPlay = !this.isPlay // 切换播放状态
     },
     musicEnd(){
@@ -100,7 +114,42 @@ export default {
 </script>
 
 <style scoped lang="scss">
+
 @import "@/assets/scss/_handle.scss";
+@keyframes wordsLoop {
+  0% {
+    transform: translateX(200px);
+    -webkit-transform: translateX(200px);
+  }
+
+  100% {
+    transform: translateX(-100%);
+    -webkit-transform: translateX(-100%);
+  }
+}
+
+
+
+@-webkit-keyframes wordsLoop {
+  0% {
+    transform: translateX(200px);
+    -webkit-transform: translateX(200px);
+  }
+  100% {
+    transform: translateX(-100%);
+    -webkit-transform: translateX(-100%);
+  }
+}
+@keyframes rotation{
+  from {-webkit-transform: rotate(0deg);}
+  to {-webkit-transform: rotate(360deg);}
+}
+.animate {
+  display: inline-block;
+  white-space: nowrap;
+  animation: 10s wordsLoop linear infinite normal;
+
+}
 .my_aplayer {
   display: flex;
   align-items: center;
@@ -108,6 +157,7 @@ export default {
   justify-content: center;
   width: 50vw;
   @include font_color("font_color1");
+  overflow: hidden;
 }
 .music_options {
   width: 50vw;
@@ -133,6 +183,26 @@ export default {
   width: 5em;
   height: 5em;
   border-radius: 50%;
-  object-fit: cover;
+
 }
+.pause{
+  @extend .music_cover;
+  animation-play-state: paused !important;
+
+  -webkit-animation-play-state: paused !important;
+}
+.text_pause {
+  animation-play-state: paused !important;
+
+  -webkit-animation-play-state: paused !important;
+}
+.toggleAudioAni{
+  @extend .music_cover;
+  -webkit-transform: rotate(360deg);
+  animation: rotation 7s linear infinite;
+  -moz-animation: rotation 7s linear infinite;
+  -webkit-animation: rotation 7s linear infinite;
+  -o-animation: rotation 7s linear infinite;
+}
+
 </style>
