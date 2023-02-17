ef5902e488fa0e93941f4ddd64db8a3e3ff8f7b7
aglorice <401208941@qq.com>
2023-02-18 01:23:58 +0800

    优化了目录生成的过程

--- src/utils/transListToTreeData.js
+++ src/utils/transListToTreeData.js
@@ -1,40 +1,50 @@
-export function transListToTreeData(list) {
-    const startIndent = list[1].indent;
-    const endIndent = list[1].indent + 1;
-    let arr = [];
-    for (let i=1;i<list.length;i++){
-        // 如果标题大小等于开始的标题
-        if (list[i].indent === startIndent){
-            arr.push({
-                label: list[i].title,
-                lineIndex:list[i].lineIndex,
-            })
-            if(list[i+1].indent>list[i].indent){
-                let children ={
-                    label: list[i+1].title,
-                    lineIndex:list[i+1].lineIndex,
-                }
-                if (arr[arr.length-1].children){
-                    arr[arr.length-1].children.push(children)
-                }else {
-                    arr[arr.length-1].children = []
-                }
-                for(let j=i+1;list[j].indent>=endIndent;j++){
-                    let children ={
-                        label: list[j].title,
-                        lineIndex:list[j].lineIndex,
-                    }
-                    if (arr[arr.length-1].children){
-                        arr[arr.length-1].children.push(children)
-                    }else {
-                        arr[arr.length-1].children = []
-                    }
-                    if (j === list.length - 1 ){
-                        break
-                    }
-                }
-            }
-        }
+function transformed_data (_data,new_data = [],new_data_list = []){
+    new_data = _data[0];
+    new_data.label = _data[0].title
+    if(_data.length === 1){
+        return new_data
+    }else {
+        _data.forEach((item)=>{
+            item.label = item.title
+            new_data_list.push(item)
+        })
+        new_data.children = new_data_list.splice(1)
     }
-    return arr
+    return new_data
 }
+
+function dispatch_data (_data,new_data=[]){
+    _data.forEach((item)=>{
+        new_data.push(transformed_data(item))
+    })
+    return new_data
+}
+
+/**
+ * 将标题数据根据标题大小分割开
+ * @param {object}data
+ * @param {number}indent
+ * @returns {*[]}
+ */
+function split_data (data,indent){
+    let rets = [];
+
+    data.forEach((item,index)=>{
+        if(item.indent === indent){
+            rets.push(index)
+        }
+    })
+
+    let newArr = [] //首先创建一个新的空数组。用来存放分割好的数组
+    for(let index = 0;index<rets.length;index++){
+        newArr.push(data.slice(rets[index], rets[index+1]));
+    }
+    return newArr
+}
+
+export  function transListToTreeData(data){
+    return dispatch_data(split_data(data,0))
+}
+
+
+
